8e07c29e1b53d59185f1ea52ec945e1a316fe69c
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2023-10-03 18:27:23 +0200

    TDD -> full cyclomatic coverage

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,9 @@
 import positive from ".";
 
 test("should behave as the identity function for positive numbers", () => {
-  expect(positive(10)).toEqual(10);
+  expect(positive(1)).toEqual(1);
+});
+
+test("should return 0 for negative numbers", () => {
+  expect(positive(-1)).toEqual(0);
 });
